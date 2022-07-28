c389cc5c8598d7da3b345c2b5d53c7349645b15a
Yaman <yamix2000@gmail.com>
2022-07-28 02:48:38 +0200

    feat(header): Add initial support for Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import ServiceCard from './components/ServiceCard/ServiceCard';
-import reactLogo from './assets/react.svg'
+import Hero from './components/Hero/Hero';
 import Footer from './components/Footer/Footer';
 
 import servicesData from './data';
 import Service from './interfaces';
+
 function App() {
   return (
     <div className="w-screen h-ful max-w-7xl text-white pb-0 m-0 mx-auto">
+      <Hero />
+
       <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-14">
         {servicesData.map((serv: Service) => (
           <ServiceCard
---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,9 @@
+const Hero = () => {
+  return (
+    <div>
+      <h1>Welcom to Adamnor Cloud Services</h1>
+    </div>
+  );
+};
+
+export default Hero;
