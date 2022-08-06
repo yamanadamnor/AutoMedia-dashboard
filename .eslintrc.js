d2a73af0a0e9ee7025ecb02d24c04f944932e283
Yaman <yamix2000@gmail.com>
2022-08-07 00:51:42 +0200

    fix(CI): Fix super-linter issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,14 @@
 module.exports = {
-  extends: ['next/core-web-vitals'],
+  extends: [
+    'eslint:recommended',
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
   rules: {
-    // note you must disable the base rule as it can report incorrect errors
-    semi: 'off',
-    '@typescript-eslint/semi': ['error'],
+    'no-console': 'error',
+    'prefer-const': 'error',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
   },
 };
