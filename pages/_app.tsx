cf498b8682a2cee19b1dca8df2239704c273437c
Yaman <yamix2000@gmail.com>
2022-10-17 11:32:41 +0200

    fix: Fix build issue

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Head from 'next/head';
 import type { AppProps } from 'next/app';
+import { Session } from 'next-auth';
 import { SessionProvider } from 'next-auth/react';
 import '../styles/global.css';
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps<{session: Session}>) {
   return (
     <>
       <Head>
