568ef258f87e0de96478e414f6e40ec1e15abd65
Yaman <yamix2000@gmail.com>
2023-03-01 17:45:35 +0100

    fixup! fixup! calendar: Update UI

--- pages/api/service/[id].ts
+++ pages/api/service/[id].ts
@@ -1,33 +1,52 @@
-import { PrismaClient } from '@prisma/client';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-const prisma = new PrismaClient();
+import { prisma } from '../../../server/prisma';
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const {
     query: { id },
     method,
   } = req;
 
-  let deletedUser = {};
+  // let handledService = {};
 
   switch (method) {
     case 'DELETE':
       if (!id) return;
       try {
-        deletedUser = await prisma.service.delete({
+        const handledService = await prisma.service.delete({
           where: {
             id: Array.isArray(id) ? parseInt(id[0]) : parseInt(id),
           },
         });
-        res.status(200).send(deletedUser);
+        res.status(200).send(handledService);
       } catch (error) {
         res.status(500).send('Could not delete service');
       }
 
       break;
 
+    case 'PUT':
+      if (!id) return;
+      try {
+        const data = req.body;
+        console.log(data);
+        const updatedService = await prisma.service.update({
+          data,
+          where: {
+            id: Array.isArray(id) ? parseInt(id[0]) : parseInt(id),
+          },
+        });
+
+        res.status(200).send(updatedService);
+      } catch (error) {
+        res.status(500).send('Could not edit service, try again');
+      }
+
+      break;
+
     default:
-      res.setHeader('Allow', ['GET', 'DELETE']);
+      res.setHeader('Allow', ['GET', 'PUT', 'DELETE']);
       res.status(405).end(`Method ${method} Not Allowed`);
       break;
   }
