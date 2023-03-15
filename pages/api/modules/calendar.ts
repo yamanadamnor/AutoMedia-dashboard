00391dcf921c429defa1a42eb3970edfd51a53a3
Yaman Adamnor <yamix2000@gmail.com>
2023-03-15 16:18:46 +0100

    ref: Update sonarr api to v3

--- pages/api/modules/calendar.ts
+++ pages/api/modules/calendar.ts
@@ -12,7 +12,7 @@ async function handlePost(req: NextApiRequest, res: NextApiResponse) {
     {
       service: 'sonarr',
       port: 8989,
-      url: '/api/calendar',
+      url: '/api/v3/calendar',
       apiKey: process.env.NEXT_PUBLIC_SONARR_API,
     },
     {
@@ -33,7 +33,9 @@ async function handlePost(req: NextApiRequest, res: NextApiResponse) {
   if (!serviceConfig) return;
   const { port, url, apiKey } = serviceConfig;
 
-  const requestUrl = `${baseUrl}:${port}${url}?apikey=${apiKey}&start=${startDate}&end=${endDate}`;
+  let requestUrl = `${baseUrl}:${port}${url}?apikey=${apiKey}&start=${startDate}&end=${endDate}`;
+
+  requestUrl += type === 'sonarr' ? '&includeSeries=true' : '';
 
   const mediaData = await fetcher(requestUrl);
   return res.json(mediaData);
