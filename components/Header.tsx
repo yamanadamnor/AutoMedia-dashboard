65c8a86d355ef2c0fdb51f6a4957b8d5f11ed989
Yaman <yamix2000@gmail.com>
2023-03-17 23:09:32 +0100

    feat: UI improvements

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ import acsLogo from '../public/img/logo-white.svg';
 const Header = () => {
   return (
     <>
-      <div className="w-12">
+      <div className="w-10">
         <Image src={acsLogo} alt="logo" />
       </div>
       <LoginBtn />
