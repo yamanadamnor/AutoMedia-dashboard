becf930cd4c18adec5e40c65197e67fa23d52b58
Yaman Adamnor <yamix2000@gmail.com>
2023-08-03 15:33:08 +0200

    feat(linting): Update eslint config
    Update eslint config to better handle typescript, nextjs and react

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,46 @@
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'next', 'prettier'],
-  rules: {
-    'no-console': 'error',
-    'prefer-const': 'error',
-    '@typescript-eslint/no-unused-vars': 'error',
-    '@typescript-eslint/no-explicit-any': 'error',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'prettier',
+    'plugin:@next/next/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+  ],
+  env: {
+    es2022: true,
+    node: true,
   },
+  rules: {
+    'no-console': ['error'],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+    ],
+    '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      { prefer: 'type-imports', fixStyle: 'separate-type-imports' },
+    ],
+    '@typescript-eslint/no-misused-promises': [2, { checksVoidReturn: { attributes: false } }],
+    'import/consistent-type-specifier-style': ['error', 'prefer-top-level'],
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'import'],
+  parserOptions: {
+    project: true,
+  },
+  ignorePatterns: [
+    '**/.eslintrc.cjs',
+    '**/*.config.js',
+    '**/*.config.cjs',
+    'packages/config/**',
+    '.next',
+    'dist',
+    'package-lock.yaml',
+  ],
 };
