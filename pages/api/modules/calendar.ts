023614202e4f6343ab601f7e9c56bc0ac02a722a
Yaman <yamix2000@gmail.com>
2023-08-11 02:10:56 +0200

    refactor(calendar): Handle errors

--- pages/api/modules/calendar.ts
+++ pages/api/modules/calendar.ts
@@ -2,49 +2,53 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { fetcher } from '../../../components/utils';
 
 async function handleGet(req: NextApiRequest, res: NextApiResponse) {
-  const { startDate, endDate, type } = req.query as {
-    startDate: string;
-    endDate: string;
-    type: 'sonarr' | 'radarr';
-  };
+  try {
+    const { startDate, endDate, type } = req.query as {
+      startDate: string;
+      endDate: string;
+      type: 'sonarr' | 'radarr';
+    };
 
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
-  const mediaServiceUrls = [
-    {
-      service: 'sonarr',
-      port: 8989,
-      url: '/api/v3/calendar',
-      apiKey: process.env.NEXT_PUBLIC_SONARR_API,
-    },
-    {
-      service: 'radarr',
-      port: 7878,
-      url: '/api/v3/calendar',
-      apiKey: process.env.NEXT_PUBLIC_RADARR_API,
-    },
-  ];
+    const mediaServiceUrls = [
+      {
+        service: 'sonarr',
+        port: 8989,
+        url: '/api/v3/calendar',
+        apiKey: process.env.NEXT_PUBLIC_SONARR_API,
+      },
+      {
+        service: 'radarr',
+        port: 7878,
+        url: '/api/v3/calendar',
+        apiKey: process.env.NEXT_PUBLIC_RADARR_API,
+      },
+    ];
 
-  if (!type) {
-    return res.status(400).json({
-      message: 'Missing required parameter in body: type',
-    });
+    if (!type) {
+      return res.status(400).json({
+        message: 'Missing required parameter in body: type',
+      });
+    }
+
+    const serviceConfig = mediaServiceUrls.find((service) => service.service === type);
+
+    if (!serviceConfig) return;
+    const { port, url, apiKey } = serviceConfig;
+
+    let requestUrl = `${baseUrl}:${port}${url}?apikey=${apiKey}&start=${startDate}&end=${endDate}`;
+
+    requestUrl += type === 'sonarr' ? '&includeSeries=true' : '';
+
+    // TODO: Fix type
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const mediaData = await fetcher(requestUrl);
+
+    return res.json(mediaData);
+  } catch {
+    return res.status(500).json({ message: 'Internal Server Error' });
   }
-
-  const serviceConfig = mediaServiceUrls.find((service) => service.service === type);
-
-  if (!serviceConfig) return;
-  const { port, url, apiKey } = serviceConfig;
-
-  let requestUrl = `${baseUrl}:${port}${url}?apikey=${apiKey}&start=${startDate}&end=${endDate}`;
-
-  requestUrl += type === 'sonarr' ? '&includeSeries=true' : '';
-
-  // TODO: Fix type
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const mediaData = await fetcher(requestUrl);
-
-  return res.json(mediaData);
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
