7f3ce2bab63fe706d2825b97e62c8481b7aa854c
Yaman <yamix2000@gmail.com>
2023-08-23 17:19:29 +0200

    feat: Add settings model and api routes

---
+++ pages/api/settings.ts
@@ -0,0 +1,40 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import { prisma } from "@/server/prisma";
+import type { SettingsFormValues } from "@/components/SettingsForm";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method === "POST") {
+    const settings = req.body as SettingsFormValues;
+
+    try {
+      for (const [key, value] of Object.entries(settings)) {
+        await prisma.setting.upsert({
+          where: { key: key },
+          update: { key: key, value: String(value) },
+          create: { key: key, value: String(value) },
+        });
+      }
+    } catch {
+      return res.status(500).send({ message: "Could not save settings" });
+    }
+
+    return res.status(201).json(settings);
+  } else if (req.method === "GET") {
+    const settings = await prisma.setting.findMany();
+    const parsedSettings = Object.fromEntries(
+      structuredClone(settings).map((item) => [
+        item.key,
+        item.value === "true" || item.value === "false"
+          ? JSON.parse(item.value)
+          : item.value,
+      ]),
+    ) as SettingsFormValues;
+    res.status(200).json(parsedSettings);
+  } else {
+    res.status(405).send({ message: "Method not allowed" });
+  }
+}
