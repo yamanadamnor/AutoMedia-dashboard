5d77404ab3718355e23da15c8391b029fcd6468b
Yaman <yamix2000@gmail.com>
2023-08-23 17:25:09 +0200

    chore(formatting): Update formatting

--- pages/api/services.ts
+++ pages/api/services.ts
@@ -1,11 +1,14 @@
-import type { Service } from '@prisma/client';
-import type { NextApiRequest, NextApiResponse } from 'next';
-import type { Prisma } from '@prisma/client';
+import type { Service } from "@prisma/client";
+import type { NextApiRequest, NextApiResponse } from "next";
+import type { Prisma } from "@prisma/client";
 
 import { prisma } from '../../server/prisma';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method === "POST") {
     const serviceData = req.body as Prisma.ServiceCreateInput;
 
     const savedService = await prisma.service.create({
@@ -13,10 +16,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     });
 
     return res.json(savedService);
-  } else if (req.method === 'GET') {
+  } else if (req.method === "GET") {
     const services: Service[] = await prisma.service.findMany();
+
     res.json(services);
   } else {
-    res.status(405).send({ message: 'Method not allowed' });
+    res.status(405).send({ message: "Method not allowed" });
   }
 }
