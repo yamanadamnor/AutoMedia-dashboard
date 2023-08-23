728b5d03ffc9e83b66df37b24a21fa1053617545
Yaman <yamix2000@gmail.com>
2023-08-23 17:19:48 +0200

    feat: Add Settings component

---
+++ components/SettingsForm.test.ts
@@ -0,0 +1,87 @@
+import { describe, it, expect } from "vitest";
+import { settingsFormSchema } from "@/components/SettingsForm";
+
+describe("test settings config", () => {
+  it("should allow no Sonarr or Radarr integrations", () => {
+    expect(
+      settingsFormSchema.safeParse({
+        theme: "dark",
+        ENABLE_SONARR: false,
+        SONARR_URL: undefined,
+        SONARR_API_KEY: undefined,
+        ENABLE_RADARR: false,
+        RADARR_URL: undefined,
+        RADARR_API_KEY: undefined,
+      }).success,
+    ).to.equal(true);
+  });
+
+  it("should not allow Sonarr integration without API key and URL", () => {
+    expect(
+      settingsFormSchema.safeParse({
+        theme: "dark",
+        ENABLE_SONARR: true,
+        SONARR_URL: undefined,
+        SONARR_API_KEY: "",
+        ENABLE_RADARR: false,
+        RADARR_URL: undefined,
+        RADARR_API_KEY: undefined,
+      }).success,
+    ).to.equal(false);
+  });
+
+  it("should allow Sonarr integration with API key and URL", () => {
+    expect(
+      settingsFormSchema.safeParse({
+        theme: "dark",
+        ENABLE_SONARR: true,
+        SONARR_URL: "http://testendpoint.test",
+        SONARR_API_KEY: "TEST_API_KEY_FOR_TESTING",
+        ENABLE_RADARR: false,
+        RADARR_URL: undefined,
+        RADARR_API_KEY: undefined,
+      }).success,
+    ).to.equal(true);
+  });
+
+  it("should not allow Radarr integration without API key and URL", () => {
+    expect(
+      settingsFormSchema.safeParse({
+        theme: "dark",
+        ENABLE_SONARR: false,
+        SONARR_URL: undefined,
+        SONARR_API_KEY: undefined,
+        ENABLE_RADARR: true,
+        RADARR_URL: "",
+        RADARR_API_KEY: "",
+      }).success,
+    ).to.equal(false);
+  });
+  it("should allow Radarr integration with API key and URL", () => {
+    expect(
+      settingsFormSchema.safeParse({
+        theme: "dark",
+        ENABLE_SONARR: true,
+        SONARR_URL: "http://testendpoint.test",
+        SONARR_API_KEY: "TEST_API_KEY_FOR_TESTING",
+        ENABLE_RADARR: false,
+        RADARR_URL: undefined,
+        RADARR_API_KEY: undefined,
+      }).success,
+    ).to.equal(true);
+  });
+
+  it("should not allow sonarr or radarr integration without API key and URL", () => {
+    expect(
+      settingsFormSchema.safeParse({
+        theme: "dark",
+        ENABLE_SONARR: true,
+        SONARR_URL: undefined,
+        SONARR_API_KEY: undefined,
+        ENABLE_RADARR: true,
+        RADARR_URL: undefined,
+        RADARR_API_KEY: undefined,
+      }).success,
+    ).to.equal(false);
+  });
+});
