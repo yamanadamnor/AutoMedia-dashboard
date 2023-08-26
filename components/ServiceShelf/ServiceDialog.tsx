2bb2ad2539d0bf8ca60d1e7b468e8f7697e57892
Yaman <yamix2000@gmail.com>
2023-08-26 05:45:52 +0200

    feat: Add ServiceDialog component
    Add tailwindcss-animate plugin

---
+++ components/ServiceShelf/ServiceDialog.tsx
@@ -0,0 +1,45 @@
+"use client";
+import * as React from "react";
+import { useAtom } from "jotai";
+
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogHeader,
+  DialogFooter,
+  DialogTrigger,
+} from "@/ui/Dialog";
+import { AddServiceModalAtom } from "@/components/states";
+import { Button } from "@/ui/Button";
+import { ServiceForm } from "@/components/ServiceShelf/ServiceForm";
+import type { ServiceFormValues } from "@/components/ServiceShelf/ServiceForm";
+
+export const ServiceDialog = ({
+  trigger,
+  service,
+}: {
+  trigger: React.ReactNode;
+  service?: ServiceFormValues;
+}) => {
+  const [open, setOpen] = useAtom(AddServiceModalAtom);
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        {trigger ? trigger : <Button type="button">Add service</Button>}
+      </DialogTrigger>
+      <DialogContent className="bg-service-card text-white backdrop-blur-lg">
+        <DialogHeader>
+          <DialogTitle>Service</DialogTitle>
+        </DialogHeader>
+        <ServiceForm service={service} />
+        <DialogFooter className="mt-5">
+          <Button form="serviceForm" type="submit">
+            {service ? "Update" : "Save"}
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,93 +1,104 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './ui/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./ui/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       maxWidth: {
-        '8xl': '90rem',
+        "8xl": "90rem",
       },
       aspectRatio: {
-        '2/3': '2 / 3',
+        "2/3": "2 / 3",
       },
       gridTemplateColumns: {
-        app: '1% 3fr repeat(6, 1fr) 1%',
-        mediaItem: 'auto 1fr',
+        app: "1% 3fr repeat(6, 1fr) 1%",
+        mediaItem: "auto 1fr",
       },
       gridTemplateRows: {
-        app: 'auto auto 1fr',
+        app: "auto auto 1fr",
       },
       backgroundImage: {
-        'main-img': "url('/img/acs-background-2.png')",
+        "main-img": "url('/img/acs-background-2.png')",
       },
       backgroundSize: {
-        blown: '200% 200%',
+        blown: "200% 200%",
       },
       colors: {
-        'service-card': '#393b4360',
-        'service-card-solid': '#21222e',
-        'light-shadow': '#2f376444',
-        'dark-shadow': '#22405A44',
-        'service-desc-dark': 'rgba(238, 238, 238, 0.404)',
-        'service-desc-light': 'rgba(238, 238, 238, 0.909)',
-        background: '#111019',
+        "service-card": "#393b4360",
+        "service-card-solid": "#21222e",
+        "light-shadow": "#2f376444",
+        "dark-shadow": "#22405A44",
+        "service-desc-dark": "rgba(238, 238, 238, 0.404)",
+        "service-desc-light": "rgba(238, 238, 238, 0.909)",
+        background: "#111019",
       },
       boxShadow: ({ theme }) => ({
-        service: `-8px -8px 30px ${theme('colors.light-shadow')}, 8px 8px 30px ${theme(
-          'colors.dark-shadow',
-        )}, inset 0 0 0 2px ${theme('colors.light-shadow')};`,
-        'service-sm': `-6px -6px 20px ${theme('colors.light-shadow')}, 6px 6px 20px ${theme(
-          'colors.dark-shadow',
-        )}, inset 0 0 0 1px ${theme('colors.light-shadow')};`,
+        service: `-8px -8px 30px ${theme(
+          "colors.light-shadow",
+        )}, 8px 8px 30px ${theme(
+          "colors.dark-shadow",
+        )}, inset 0 0 0 2px ${theme("colors.light-shadow")};`,
+        "service-sm": `-6px -6px 20px ${theme(
+          "colors.light-shadow",
+        )}, 6px 6px 20px ${theme(
+          "colors.dark-shadow",
+        )}, inset 0 0 0 1px ${theme("colors.light-shadow")};`,
       }),
       fontFamily: {
-        'gt-walsheim-pro': ['GT Walsheim Pro'],
-        sans: ['GT Walsheim Pro', ...defaultTheme.fontFamily.sans],
+        "gt-walsheim-pro": ["GT Walsheim Pro"],
+        sans: ["GT Walsheim Pro", ...defaultTheme.fontFamily.sans],
       },
       lineHeight: {
-        20: '5rem',
+        20: "5rem",
       },
       keyframes: {
         move: {
-          '0%, 100%': { backgroundPosition: '0% 0%' },
-          '50%': { backgroundPosition: '100% 0%' },
+          "0%, 100%": { backgroundPosition: "0% 0%" },
+          "50%": { backgroundPosition: "100% 0%" },
         },
 
         slideDownAndFade: {
-          from: { opacity: 0, transform: 'translateY(-2px)' },
-          to: { opacity: 1, transform: 'translateY(0)' },
+          from: { opacity: 0, transform: "translateY(-2px)" },
+          to: { opacity: 1, transform: "translateY(0)" },
         },
         slideLeftAndFade: {
-          from: { opacity: 0, transform: 'translateX(2px)' },
-          to: { opacity: 1, transform: 'translateX(0)' },
+          from: { opacity: 0, transform: "translateX(2px)" },
+          to: { opacity: 1, transform: "translateX(0)" },
         },
         slideUpAndFade: {
-          from: { opacity: 0, transform: 'translateY(2px)' },
-          to: { opacity: 1, transform: 'translateY(0)' },
+          from: { opacity: 0, transform: "translateY(2px)" },
+          to: { opacity: 1, transform: "translateY(0)" },
         },
         slideRightAndFade: {
-          from: { opacity: 0, transform: 'translateX(-2px)' },
-          to: { opacity: 1, transform: 'translateX(0)' },
+          from: { opacity: 0, transform: "translateX(-2px)" },
+          to: { opacity: 1, transform: "translateX(0)" },
         },
       },
       animation: {
-        move: 'move 2s ease-in-out infinite',
-        slideDownAndFade: 'slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
-        slideLeftAndFade: 'slideLeftAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
-        slideUpAndFade: 'slideUpAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
-        slideRightAndFade: 'slideRightAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+        move: "move 2s ease-in-out infinite",
+        slideDownAndFade:
+          "slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
+        slideLeftAndFade:
+          "slideLeftAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
+        slideUpAndFade: "slideUpAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
+        slideRightAndFade:
+          "slideRightAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)",
       },
     },
     debugScreens: {
-      position: ['bottom', 'right'],
+      position: ["bottom", "right"],
     },
   },
-  plugins: [require('@tailwindcss/container-queries'), require('tailwindcss-debug-screens')],
+  plugins: [
+    require("@tailwindcss/container-queries"),
+    require("tailwindcss-debug-screens"),
+    require("tailwindcss-animate"),
+  ],
 };
