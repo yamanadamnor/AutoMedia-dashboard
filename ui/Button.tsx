f0782cb318dbfb6278aad7fad7cd8a9ed3e3235e
Yaman <yamix2000@gmail.com>
2023-08-26 06:14:10 +0200

    feat(Button): Add styling for disabled state

--- ui/Button.tsx
+++ ui/Button.tsx
@@ -11,7 +11,7 @@ export const Button = React.forwardRef<ButtonElement, ButtonElementProps>(
         ref={ref}
         type="button"
         className={cn(
-          "box-border rounded-md border border-zinc-700 px-6 py-2 text-service-desc-light transition-all duration-200 ease-in-out hover:border-zinc-400 hover:bg-service-card hover:text-gray-200",
+          "box-border select-none rounded-md border border-zinc-700 px-6 py-2 text-service-desc-light transition-all duration-200 ease-in-out hover:border-zinc-400 hover:bg-service-card hover:text-gray-200 disabled:border-zinc-800 disabled:text-zinc-600 disabled:hover:bg-transparent",
           className,
         )}
         {...props}
