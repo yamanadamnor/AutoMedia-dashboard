f85af90973d7f58636d7b2c438aa4dff49c96eaa
Yaman <yamix2000@gmail.com>
2023-08-26 05:56:03 +0200

    feat: Add fetcher and poster utils

---
+++ utils/fetcher.ts
@@ -0,0 +1,7 @@
+// Function used for sending GET requests
+export const fetcher = (url: string) => {
+  return fetch(url, {
+    method: "GET",
+    headers: { "content-type": "application/json" },
+  }).then((res) => res.json());
+};
