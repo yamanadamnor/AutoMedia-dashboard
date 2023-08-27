d9f335bf03a567e330c42fb1b4b31cbe8e738786
Yaman <yamix2000@gmail.com>
2023-08-27 19:52:27 +0200

    refactor(SettingsForm): Rework SettingsForm validation
    Fix validation by using superRefine instead of refine
    This makes it possible to handle errors for each field

--- components/SettingsForm.test.ts
+++ components/SettingsForm.test.ts
@@ -2,19 +2,18 @@ import { describe, it, expect } from "vitest";
 import { settingsFormSchema } from "@/components/SettingsForm";
 
 describe("test settings config", () => {
-  it("should allow no Sonarr or Radarr integrations", () => {
+  it("should allow no Sonarr, Radarr or Jellyfin integrations", () => {
     expect(
       settingsFormSchema.safeParse({
-        theme: "dark",
         ENABLE_SONARR: false,
-        SONARR_URL: undefined,
-        SONARR_API_KEY: undefined,
+        SONARR_URL: "not valid url",
+        SONARR_API_KEY: "api not key",
         ENABLE_RADARR: false,
-        RADARR_URL: undefined,
-        RADARR_API_KEY: undefined,
+        RADARR_URL: "not valid url",
+        RADARR_API_KEY: "api not key",
         ENABLE_JELLYFIN: false,
-        JELLYFIN_URL: undefined,
-        JELLYFIN_API_KEY: undefined,
+        JELLYFIN_URL: "not valid url",
+        JELLYFIN_API_KEY: "api not key",
       }).success,
     ).to.equal(true);
   });
@@ -22,16 +21,15 @@ describe("test settings config", () => {
   it("should not allow Sonarr integration without API key and URL", () => {
     expect(
       settingsFormSchema.safeParse({
-        theme: "dark",
         ENABLE_SONARR: true,
-        SONARR_URL: undefined,
+        SONARR_URL: "",
         SONARR_API_KEY: "",
         ENABLE_RADARR: false,
-        RADARR_URL: undefined,
-        RADARR_API_KEY: undefined,
+        RADARR_URL: "",
+        RADARR_API_KEY: "",
         ENABLE_JELLYFIN: false,
-        JELLYFIN_URL: undefined,
-        JELLYFIN_API_KEY: undefined,
+        JELLYFIN_URL: "",
+        JELLYFIN_API_KEY: "",
       }).success,
     ).to.equal(false);
   });
@@ -39,16 +37,15 @@ describe("test settings config", () => {
   it("should allow Sonarr integration with API key and URL", () => {
     expect(
       settingsFormSchema.safeParse({
-        theme: "dark",
         ENABLE_SONARR: true,
         SONARR_URL: "http://testendpoint.test",
         SONARR_API_KEY: "TEST_API_KEY_FOR_TESTING",
         ENABLE_RADARR: false,
-        RADARR_URL: undefined,
-        RADARR_API_KEY: undefined,
+        RADARR_URL: "",
+        RADARR_API_KEY: "",
         ENABLE_JELLYFIN: false,
-        JELLYFIN_URL: undefined,
-        JELLYFIN_API_KEY: undefined,
+        JELLYFIN_URL: "",
+        JELLYFIN_API_KEY: "",
       }).success,
     ).to.equal(true);
   });
@@ -56,49 +53,78 @@ describe("test settings config", () => {
   it("should not allow Radarr integration without API key and URL", () => {
     expect(
       settingsFormSchema.safeParse({
-        theme: "dark",
         ENABLE_SONARR: false,
-        SONARR_URL: undefined,
-        SONARR_API_KEY: undefined,
+        SONARR_URL: "",
+        SONARR_API_KEY: "",
         ENABLE_RADARR: true,
         RADARR_URL: "",
         RADARR_API_KEY: "",
         ENABLE_JELLYFIN: false,
-        JELLYFIN_URL: undefined,
-        JELLYFIN_API_KEY: undefined,
+        JELLYFIN_URL: "",
+        JELLYFIN_API_KEY: "",
       }).success,
     ).to.equal(false);
   });
   it("should allow Radarr integration with API key and URL", () => {
     expect(
       settingsFormSchema.safeParse({
-        theme: "dark",
-        ENABLE_SONARR: true,
-        SONARR_URL: "http://testendpoint.test",
-        SONARR_API_KEY: "TEST_API_KEY_FOR_TESTING",
-        ENABLE_RADARR: false,
-        RADARR_URL: undefined,
-        RADARR_API_KEY: undefined,
+        ENABLE_SONARR: false,
+        SONARR_URL: "",
+        SONARR_API_KEY: "",
+        ENABLE_RADARR: true,
+        RADARR_URL: "http://testendpoint.test",
+        RADARR_API_KEY: "TEST_API_KEY_FOR_TESTING",
         ENABLE_JELLYFIN: false,
-        JELLYFIN_URL: undefined,
-        JELLYFIN_API_KEY: undefined,
+        JELLYFIN_URL: "",
+        JELLYFIN_API_KEY: "",
       }).success,
     ).to.equal(true);
   });
 
-  it("should not allow sonarr or radarr integration without API key and URL", () => {
+  it("should not allow Jellyfin integration without API key and URL", () => {
     expect(
       settingsFormSchema.safeParse({
-        theme: "dark",
+        ENABLE_SONARR: false,
+        SONARR_URL: "",
+        SONARR_API_KEY: "",
+        ENABLE_RADARR: false,
+        RADARR_URL: "",
+        RADARR_API_KEY: "",
+        ENABLE_JELLYFIN: true,
+        JELLYFIN_URL: "",
+        JELLYFIN_API_KEY: "",
+      }).success,
+    ).to.equal(false);
+  });
+
+  it("should allow Jellyfin integration with API key and URL", () => {
+    expect(
+      settingsFormSchema.safeParse({
+        ENABLE_SONARR: false,
+        SONARR_URL: "",
+        SONARR_API_KEY: "",
+        ENABLE_RADARR: false,
+        RADARR_URL: "",
+        RADARR_API_KEY: "",
+        ENABLE_JELLYFIN: true,
+        JELLYFIN_URL: "http://testendpoint.test",
+        JELLYFIN_API_KEY: "TEST_API_KEY_FOR_TESTING",
+      }).success,
+    ).to.equal(true);
+  });
+
+  it("should not allow Sonarr, Radarr or Jellyfin integration without API key and URL", () => {
+    expect(
+      settingsFormSchema.safeParse({
         ENABLE_SONARR: true,
-        SONARR_URL: undefined,
-        SONARR_API_KEY: undefined,
+        SONARR_URL: "",
+        SONARR_API_KEY: "",
         ENABLE_RADARR: true,
-        RADARR_URL: undefined,
-        RADARR_API_KEY: undefined,
-        ENABLE_JELLYFIN: false,
-        JELLYFIN_URL: undefined,
-        JELLYFIN_API_KEY: undefined,
+        RADARR_URL: "",
+        RADARR_API_KEY: "",
+        ENABLE_JELLYFIN: true,
+        JELLYFIN_URL: "",
+        JELLYFIN_API_KEY: "",
       }).success,
     ).to.equal(false);
   });
