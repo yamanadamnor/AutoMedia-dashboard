177a3d9a6994f7a5ff17e58efd353f1b76e8755f
Yaman Adamnor <32465405+yamanadamnor@users.noreply.github.com>
2024-08-22 09:53:44 +0200

    feat: Add toaster support

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,9 +8,17 @@ import {
   Hero,
 } from "@/components/ClientComponents";
 import { SessionProvider } from "next-auth/react";
+import { Toaster } from "react-hot-toast";
+import { getServices } from "@/data/service";
 import { TailwindBreakpointIndicator } from "@/components/TailwindBreakpointIndicator";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const services = await getServices();
+
   return (
     <html lang="en" className="bg-base h-full">
       <body>
@@ -24,7 +32,26 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           </div>
         </div>
         <SessionProvider>
-          <div className="center justify-items-centerr text-whitelg:gap-x-8 relative z-auto mx-auto grid h-full min-h-screen max-w-8xl grid-cols-app grid-rows-app place-content-start gap-x-6 gap-y-8">
+          <div className="center justify-items-centerr relative z-auto mx-auto grid h-full min-h-screen max-w-8xl grid-cols-app grid-rows-app place-content-start gap-x-6 gap-y-8 text-white lg:gap-x-8">
+            <Toaster
+              position="top-right"
+              toastOptions={{
+                success: {
+                  style: {
+                    borderRadius: "10px",
+                    background: "#20202c",
+                    color: "#fff",
+                  },
+                },
+                error: {
+                  style: {
+                    borderRadius: "10px",
+                    background: "#20202c",
+                    color: "#fff",
+                  },
+                },
+              }}
+            />
             <div className="col-span-7 col-start-2 row-start-1 row-end-2 flex h-24 w-full items-center justify-between py-6">
               <Header services={services} />
             </div>
