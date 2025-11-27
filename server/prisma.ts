4527934be73dc6f8cc1a26ccfa90416abb8524e6
Yaman Adamnor <32465405+yamanadamnor@users.noreply.github.com>
2025-11-27 18:52:41 +0100

    feat: Migrate to prisma v7

--- server/prisma.ts
+++ server/prisma.ts
@@ -1,15 +1,8 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import { PrismaClient } from "../prisma/generated/client";
 
-const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
+const adapter = new PrismaBetterSqlite3({
+	url: process.env.DATABASE_URL || "file:../config/dashboard.db",
+});
 
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
-
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
-
-export { prisma };
-
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
+export const prisma = new PrismaClient({ adapter });
