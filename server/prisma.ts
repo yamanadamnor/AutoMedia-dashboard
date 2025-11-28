35ef0045f601076a38a0b4f1b6f99dce03daa96a
Yaman Adamnor <32465405+yamanadamnor@users.noreply.github.com>
2025-11-28 15:14:13 +0100

    refactor: Remove DATABASE_URL

--- server/prisma.ts
+++ server/prisma.ts
@@ -2,7 +2,7 @@ import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import { PrismaClient } from "../prisma/generated/client";
 
 const adapter = new PrismaBetterSqlite3({
-	url: process.env.DATABASE_URL || "file:../config/dashboard.db",
+	url: "file:../config/dashboard.db",
 });
 
 export const prisma = new PrismaClient({ adapter });
