43d0bacfe5aa68440f2807ee3a274d1ff9ced197
Yaman Adamnor <32465405+yamanadamnor@users.noreply.github.com>
2025-11-28 15:13:07 +0100

    refactor: Remove DATABASE_URL

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,5 +1,4 @@
-import "dotenv/config";
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from "prisma/config";
 
 export default defineConfig({
 	schema: "prisma/schema.prisma",
@@ -8,6 +7,6 @@ export default defineConfig({
 		seed: "tsx prisma/seed.ts",
 	},
 	datasource: {
-		url: env("DATABASE_URL"),
+		url: "file:../config/dashboard.db",
 	},
 });
