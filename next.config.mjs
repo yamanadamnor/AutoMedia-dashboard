487b220ca36bdac0e3ca2e7043bb2e55907625f9
Yaman Adamnor <32465405+yamanadamnor@users.noreply.github.com>
2025-11-28 01:12:10 +0100

    refactor: Use mjs for nextjs config

---
+++ next.config.mjs
@@ -0,0 +1,27 @@
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
+	reactStrictMode: true,
+	output: "standalone",
+	images: {
+		remotePatterns: [
+			{
+				hostname: "raw.githubusercontent.com",
+			},
+			{
+				hostname: "image.tmdb.org",
+			},
+			{
+				hostname: "artworks.thetvdb.com",
+			},
+			{
+				hostname: "cdn.jsdelivr.net",
+			},
+		],
+	},
+};
+
+export default nextConfig;
