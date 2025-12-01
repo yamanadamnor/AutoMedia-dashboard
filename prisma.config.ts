367665906ed00fe23a8e50b84b9abb71a094338b
Yaman Adamnor <32465405+yamanadamnor@users.noreply.github.com>
2025-12-01 14:18:39 +0100

    fix: Update prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,10 +3,10 @@ import { defineConfig } from "prisma/config";
 export default defineConfig({
 	schema: "prisma/schema.prisma",
 	migrations: {
-		path: "prisma/migrations",
-		seed: "tsx prisma/seed.ts",
+		path: "src/prisma/migrations",
+		seed: "tsx src/prisma/seed.ts",
 	},
 	datasource: {
-		url: "file:../config/dashboard.db",
+		url: "file:../../config/dashboard.db",
 	},
 });
