c1da17dd5cf193115115b5d0df115649e384c044
Yaman Adamnor <32465405+yamanadamnor@users.noreply.github.com>
2025-12-30 19:23:16 +0100

    refactor: Remove prisma related files

--- src/prisma/migrations/20220814212302_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Service" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "title" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-    "image" TEXT NOT NULL,
-    "href" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
