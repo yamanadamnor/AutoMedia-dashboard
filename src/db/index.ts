d74236933cbf28ba927ba347fe46a0377e348c59
Yaman Adamnor <32465405+yamanadamnor@users.noreply.github.com>
2026-01-24 15:41:04 +0100

    feat: Ensure database folder is created

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,13 +1,20 @@
-import "dotenv/config";
 import fs from "node:fs";
 import path from "node:path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import "dotenv/config";
 import { createClient } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
 
 const dbPath = "./config/dashboard.db";
+function createDatabaseFolder() {
+	const __filename = fileURLToPath(import.meta.url);
+	const __dirname = dirname(__filename);
+	const databaseFolder = path.join(__dirname, "../../config");
+	fs.mkdirSync(databaseFolder, { recursive: true });
+}
 
-// Ensure folder exists
-fs.mkdirSync(path.dirname(dbPath), { recursive: true });
+createDatabaseFolder();
 
 const client = createClient({ url: `file:${dbPath}` });
 export const db = drizzle({ client });
