e77a5d277991e376d99b37e0b88815e94d0aa633
Yaman Adamnor <32465405+yamanadamnor@users.noreply.github.com>
2026-01-24 19:38:30 +0100

    refactor: use env var

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,6 +5,6 @@ export default defineConfig({
 	schema: "./src/db/schema.ts",
 	dialect: "sqlite",
 	dbCredentials: {
-		url: "file:./config/dashboard.db",
+		url: process.env.DB_FILE_NAME!,
 	},
 });
