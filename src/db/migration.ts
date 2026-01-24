2d9e81c7d9eb8eea1d3fe212315cba6b80abbf54
Yaman Adamnor <32465405+yamanadamnor@users.noreply.github.com>
2026-01-24 15:41:15 +0100

    refactor: Update migration script

--- src/db/migration.ts
+++ src/db/migration.ts
@@ -1,11 +1,20 @@
+import fs from "node:fs";
+import path from "node:path";
 import { migrate } from "drizzle-orm/libsql/migrator";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 import { db } from ".";
 
 async function runMigrations() {
 	console.log("Migration started");
 
+	const __filename = fileURLToPath(import.meta.url);
+	const __dirname = dirname(__filename);
+
+	const migrationsFolder = path.join(__dirname, "../drizzle");
+
 	try {
-		await migrate(db, { migrationsFolder: "../drizzle" });
+		await migrate(db, { migrationsFolder });
 		console.log("Migration completed");
 	} catch (error) {
 		console.error("Migration failed:", error);
