dd86d0a5be7f83518f5c82b8250a09311a026b44
Yaman Adamnor <32465405+yamanadamnor@users.noreply.github.com>
2026-01-24 19:38:07 +0100

    refactor: Remove folder creation from db init

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,19 +1,6 @@
-import fs from "node:fs";
-import path, { dirname } from "node:path";
-import { fileURLToPath } from "node:url";
 import "dotenv/config";
 import { createClient } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
 
-const dbPath = "./config/dashboard.db";
-function createDatabaseFolder() {
-	const __filename = fileURLToPath(import.meta.url);
-	const __dirname = dirname(__filename);
-	const databaseFolder = path.join(__dirname, "../../config");
-	fs.mkdirSync(databaseFolder, { recursive: true });
-}
-
-createDatabaseFolder();
-
-const client = createClient({ url: `file:${dbPath}` });
+const client = createClient({ url: process.env.DB_FILE_NAME! });
 export const db = drizzle({ client });
