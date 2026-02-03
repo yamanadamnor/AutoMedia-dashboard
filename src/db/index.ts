36884414ee50b0d63013fd81a0037a8eff6da2eb
Yaman Adamnor <32465405+yamanadamnor@users.noreply.github.com>
2026-02-03 11:46:03 +0100

    fix: lint errors

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,6 @@
-import 'dotenv/config'
+import "dotenv/config";
 import Database from "better-sqlite3";
 import { drizzle } from "drizzle-orm/better-sqlite3";
 
-console.log("Database file: ", process.env.DB_FILE_NAME!)
 const sqlite = new Database(process.env.DB_FILE_NAME!);
 export const db = drizzle({ client: sqlite });
